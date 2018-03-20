5ba7953a4c866be9995d9450931189ea031c4063
Leland Takamine <leland@uber.com>
2018-03-20 15:37:33 -0700

    Update version to 0.0.7.

--- nanoscope.rb
+++ nanoscope.rb
@@ -1,9 +1,9 @@
 class Nanoscope < Formula
-  version "0.0.6"
+  version "0.0.7"
   desc "NanoScope"
   homepage "https://github.com/uber/nanoscope"
-  url "https://s3-us-west-2.amazonaws.com/uber-common-public/nanoscope/nanoscope-0.0.6.zip"
-  sha256 "e8d147507603c1c1549a01915b76d1734e58aa48ba021c6ae0cbd014b469881e"
+  url "https://s3-us-west-2.amazonaws.com/uber-common-public/nanoscope/nanoscope-0.0.7.zip"
+  sha256 "09b9192f42236fac31e2ace6ca03b53f1d695c609bba56152f0b0823f9f7c8cf"
 
   bottle :unneeded
 
