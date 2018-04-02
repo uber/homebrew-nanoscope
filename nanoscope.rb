76522d1358d080af7dff6255684d816e19d683fb
Leland-Takamine <leland.takamine@gmail.com>
2018-04-02 11:31:19 -0700

    Update nanoscope.rb

--- nanoscope.rb
+++ nanoscope.rb
@@ -1,6 +1,6 @@
 class Nanoscope < Formula
   version "0.2.4"
-  desc "NanoScope"
+  desc "Nanoscope"
   homepage "https://github.com/uber/nanoscope"
   url "https://s3-us-west-2.amazonaws.com/uber-common-public/nanoscope/nanoscope-0.2.4.zip"
   sha256 "8a35b27a845a5815e2c608688f8dff4daa84c274388820f9f3f9b8ecd37a38b4"
