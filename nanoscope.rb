e85a4adf3da544a2de7a49f49827b075472b88a2
Leland Takamine <leland@uber.com>
2018-04-03 12:56:35 -0700

    Update version to 0.2.6.

--- nanoscope.rb
+++ nanoscope.rb
@@ -1,14 +1,14 @@
 class Nanoscope < Formula
-  version "0.2.5"
+  version "0.2.6"
   desc "Nanoscope"
   homepage "https://github.com/uber/nanoscope"
-  url "https://s3-us-west-2.amazonaws.com/uber-common-public/nanoscope/nanoscope-0.2.5.zip"
-  sha256 "220712df2b381022d093e6a4d52e5bf3a52de4b80e0195c7a149c204cff58e71"
+  url "https://s3-us-west-2.amazonaws.com/uber-common-public/nanoscope/nanoscope-0.2.6.zip"
+  sha256 "5c77bb6c59bdf44c3573a3bd709020e320890c2623972bc6c21fadc953bb7744"
 
   bottle :unneeded
 
   def install
-    bin.install "bin/nanoscope"
+    bin.install "bin/nanoscopew" => "nanoscope"
     prefix.install Dir["lib"]
   end
 end
